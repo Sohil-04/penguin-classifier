011f46891c7ba9135f95b25d38fcbbac3a436e49
Sohil Tadavi <147256425+Sohil-04@users.noreply.github.com>
2025-08-22 17:18:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,7 @@ st.title('machine learning app ')
 
 st.info('This app build machine learning model !')
 
-data = pd.read_csv("penguins_cleaned.csv")
-data
+with st.expander('Data'):
+  st.write('***Raw Data***')
+  data = pd.read_csv("penguins_cleaned.csv")
+  data
